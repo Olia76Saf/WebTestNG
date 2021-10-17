ef615b109e7226c499d3395c95d3d5d7259e8a93
System Administrator <root@MacBook-van-Olga.local>
2021-10-17 17:07:24 +0200

    WebTestNG project first commit

---
+++ src/test/java/Web2Test.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Web2Test {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Applications/Utilities/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+
+    }
+}
